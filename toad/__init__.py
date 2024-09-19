21751f7dcbd23de7f42c480412d3f3068df01dfe
kobedemaeyer <140562329+kobedemaeyer@users.noreply.github.com>
2024-09-19 09:52:00 +0200

    Adapt to multivariate TOAD (xr.datasets)

--- toad/__init__.py
+++ toad/__init__.py
@@ -9,7 +9,7 @@ from .clustering import dbscan
 from .clustering.cluster import Clustering
 from .utils import infer_dims
 
-from _version import __version__
+#from _version import __version__
 
 # Each new abrupt shift detection method needs to register the function which
 # maps the analysis to xr.DataArray 
@@ -23,12 +23,11 @@ _clustering_methods = {
     'dbscan': dbscan.cluster
 } 
 
+# Adapted for multivariate TOAD
 def detect(
-        data: Union[xr.Dataset, xr.DataArray],
+        data: xr.Dataset,
         temporal_dim: str,
         method: str,
-        var: str = None,
-        keep_other_vars : bool = False, 
         method_kwargs={}
     ) -> xr.Dataset :
     """Map an abrupt shift detection algorithm to the dataset in the temporal
@@ -36,24 +35,14 @@ def detect(
 
     Parameters
     ----------
-    data : xr.Dataset or xr.DataArray
-        Data with two spatial and one temporal dimension. If `data` is an
-        xr.Dataset, `var` needs to be provided.
+    data : xr.Dataset
+        Data with two spatial and one temporal dimension.
     temporal_dim : str
         Specifies the dimension along which the one-dimensional time-series
         analysis for abrupt shifts is executed. Usually the time axis but could
         also be the forcing.
     method : {'asdetect'} 
         One-dimensional time-series analysis algorithm to use.
-    var : str, optional
-        Must be used in combination with `data` being an xr.Dataset. Since the
-        algorithms work on xr.DataArrays, it is needed to specify here which
-        variable to extract from the xr.Dataset.
-    keep_other_vars : bool, optional
-        Can be provided if `data` is an xr.Dataset. If True, the resulting
-        xr.DataArray is appended to the xr.Dataset. Defaults to False, such that
-        the xr.Dataset variables which are not analysed (i.e. all others than
-        `var`) are discarded from the resulting xr.Dataset.
     method_kwargs : dict, optional
         Kwargs that need to be specifically passed to the analysing algorithm.
 
@@ -78,16 +67,9 @@ def detect(
     logging.info(f'looking up detector {method}')
     detector = _detection_methods[method]
 
-    if var:
-        assert type(data) == xr.Dataset, \
-                 'Using var requires type(data) == xr.DataSet!'
-        logging.info(f'extracting variable {var} from Dataset')
-        data_array = data.get(var) 
-    else:
-        assert type(data) == xr.DataArray, 'Please provide var or an xr.DataArray!'
-        data_array = data
+    assert type(data) == xr.Dataset, 'Please provide xr.Dataset!'
 
-    assert data_array.ndim == 3, 'data must be 3-dimensional!'
+    assert dataset.ndim == 3, 'data must be 3-dimensional!'
 
     # Application of a detector results in an xr.DataArray with 
     # coords = (<temporal_dim>, 2 spatial dimensions)
@@ -96,30 +78,115 @@ def detect(
     #   as_var (<temporal_dim>, SD1, SD2)
     #   as_types_var (<temporal_dim>, SD1, SD2)
     logging.info(f'applying detector {method} to data')
-    data_array_dts = detector(
-        data=data_array, 
+    dataset_dts = detector(
+        data=dataset, 
         temporal_dim=temporal_dim,
         **method_kwargs
     )
 
     # Save gitversion to dataset
-    data_array_dts.attrs[f'{var}_git_detect'] = __version__
+    #data_array_dts.attrs[f'{var}_git_detect'] = __version__
 
-    # If True, dataset_with_as is merged into data. Else, only return dataarray
-    # with its dts together as one dataset.
-    if keep_other_vars:
-        assert type(data) == xr.Dataset, \
-                'Using keep_other_vars requires type(data) == xr.DataSet!'
-        logging.info(f'merging new variable {var}_dts into dataset')
-        dataset_with_as = xr.merge(
-            [data, data_array_dts])
-    else:
-        logging.info(f'merging {var} and {var}_dts')
-        dataset_with_as = xr.merge(
-            [data_array , data_array_dts])
-        dataset_with_as.attrs = []
+    return dataset_dts
 
-    return dataset_with_as
+# ORIGINAL
+# def detect(
+#         data: Union[xr.Dataset, xr.DataArray],
+#         temporal_dim: str,
+#         method: str,
+#         var: str = None,
+#         keep_other_vars : bool = False, 
+#         method_kwargs={}
+#     ) -> xr.Dataset :
+#     """Map an abrupt shift detection algorithm to the dataset in the temporal
+#     dimension.
+
+#     Parameters
+#     ----------
+#     data : xr.Dataset or xr.DataArray
+#         Data with two spatial and one temporal dimension. If `data` is an
+#         xr.Dataset, `var` needs to be provided.
+#     temporal_dim : str
+#         Specifies the dimension along which the one-dimensional time-series
+#         analysis for abrupt shifts is executed. Usually the time axis but could
+#         also be the forcing.
+#     method : {'asdetect'} 
+#         One-dimensional time-series analysis algorithm to use.
+#     var : str, optional
+#         Must be used in combination with `data` being an xr.Dataset. Since the
+#         algorithms work on xr.DataArrays, it is needed to specify here which
+#         variable to extract from the xr.Dataset.
+#     keep_other_vars : bool, optional
+#         Can be provided if `data` is an xr.Dataset. If True, the resulting
+#         xr.DataArray is appended to the xr.Dataset. Defaults to False, such that
+#         the xr.Dataset variables which are not analysed (i.e. all others than
+#         `var`) are discarded from the resulting xr.Dataset.
+#     method_kwargs : dict, optional
+#         Kwargs that need to be specifically passed to the analysing algorithm.
+
+#     Returns
+#     -------
+#     dataset_with_as : xr.Dataset
+#         Dataset with (at least) these variables of same dimensions and lengths: 
+#             * `var` : original variable data, 
+#             * `as_var` : Nonzero values denote an AS with the value
+#               corresponding to its magnitude,
+#         The attributes are
+#             * `as_detection_method` : details on the used as detection method
+#         If `keep_other_vars` is True, then these results are complemented by the
+#         unprocessed variables and attributes of the original `data`.
+
+#     See also
+#     --------
+#     toad.tsanalysis : Collection of abrupt shift detection algorithms 
+#     toad.clustering: Clustering algorithms using the results of the detection
+
+#     """
+#     logging.info(f'looking up detector {method}')
+#     detector = _detection_methods[method]
+
+#     if var:
+#         assert type(data) == xr.Dataset, \
+#                  'Using var requires type(data) == xr.DataSet!'
+#         logging.info(f'extracting variable {var} from Dataset')
+#         data_array = data.get(var) 
+#     else:
+#         assert type(data) == xr.DataArray, 'Please provide var or an xr.DataArray!'
+#         data_array = data
+
+#     assert data_array.ndim == 3, 'data must be 3-dimensional!'
+
+#     # Application of a detector results in an xr.DataArray with 
+#     # coords = (<temporal_dim>, 2 spatial dimensions)
+#     # variables
+#     #   var (<temporal_dim>, SD1, SD2)
+#     #   as_var (<temporal_dim>, SD1, SD2)
+#     #   as_types_var (<temporal_dim>, SD1, SD2)
+#     logging.info(f'applying detector {method} to data')
+#     data_array_dts = detector(
+#         data=data_array, 
+#         temporal_dim=temporal_dim,
+#         **method_kwargs
+#     )
+
+#     # Save gitversion to dataset
+#     #data_array_dts.attrs[f'{var}_git_detect'] = __version__
+
+#     # If True, dataset_with_as is merged into data. Else, only return dataarray
+#     # with its dts together as one dataset.
+#     if keep_other_vars:
+#         assert type(data) == xr.Dataset, \
+#                 'Using keep_other_vars requires type(data) == xr.DataSet!'
+#         logging.info(f'merging new variable {var}_dts into dataset')
+#         dataset_with_as = xr.merge(
+#             [data, data_array_dts])
+#     else:
+#         logging.info(f'merging {var} and {var}_dts')
+#         dataset_with_as = xr.merge(
+#             [data_array , data_array_dts])
+#         dataset_with_as.attrs = []
+
+#     return dataset_with_as
 
 def cluster(
         data: xr.Dataset,
@@ -145,7 +212,7 @@ def cluster(
     )
 
     # Save gitversion to dataset
-    dataset_with_clusterlabels.attrs[f'{var}_git_cluster'] = __version__
+    #dataset_with_clusterlabels.attrs[f'{var}_git_cluster'] = __version__
 
     return dataset_with_clusterlabels    
 

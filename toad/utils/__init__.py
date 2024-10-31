9a17bad9dc378a00d73ed51a29d3ee989b12e178
Jakob Harteg <github.0javt@aleeas.com>
2024-10-31 09:18:43 +0100

    Docstring for infer_dims

--- toad/utils/__init__.py
+++ toad/utils/__init__.py
@@ -1,4 +1,36 @@
 def infer_dims(xr_da, tdim=None):
+    """
+    Infers the temporal and spatial dimensions from an xarray DataArray.
+
+    Parameters:
+    -----------
+    xr_da : xarray.DataArray
+        The input DataArray from which to infer dimensions.
+    tdim : str, optional
+        The name of the temporal dimension. If provided, it will be used to 
+        distinguish between temporal and spatial dimensions. If not provided, 
+        the function will attempt to auto-detect the temporal dimension based 
+        on standard spatial dimension names.
+
+    Returns:
+    --------
+    tuple
+        A tuple containing the temporal dimension (str) and a list of spatial 
+        dimensions (list of str).
+
+    Raises:
+    -------
+    AssertionError
+        If the provided temporal dimension is not in the dimensions of the dataset.
+
+    Notes:
+    ------
+    - If `tdim` is provided, the function will use it to identify the temporal 
+      dimension and consider all other dimensions as spatial.
+    - If `tdim` is not provided, the function will attempt to auto-detect the 
+      temporal dimension by looking for standard spatial dimension pairs such as 
+      ('x', 'y'), ('lat', 'lon'), or ('latitude', 'longitude').
+    """
 
     # spatial dims are all non-temporal dims
     if tdim:

9e9e9e6e13da6d6b4c1abf479939b1a5cc7b73af
Jakob Harteg <github.0javt@aleeas.com>
2024-11-26 13:31:06 +0100

    Remove check_for_coordinate_duplicates for now

--- toad_lab/preprocessing/__init__.py
+++ toad_lab/preprocessing/__init__.py
@@ -1,16 +1,12 @@
 import xarray as xr
+import numpy as np
 
 
 class Preprocess:
     def __init__(self, toad):
         self.td = toad
-        
-    def check_for_coordinate_duplicates(self):
-        """Check if data contains duplicate values for any coordinates."""
-        dims = list(self.td.data.sizes.keys())
-        duplicates = len(self.td.data.to_dataframe().groupby(dims).size().value_counts().values) > 1
-        if duplicates:
-            print("Data contains non-unqiue values for some coordinates. This may cause unexpected behavior in TOAD. Can be mitigated with xr.drop_duplicates(dim).")
-        return duplicates
-    
 
+    def apply_xmip_conventions(self):
+        """Apply XMIP conventions to the data."""
+        raise NotImplementedError("This function is not yet implemented.")
+    

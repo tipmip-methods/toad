cf08f351c3ddb0903b494327846f0149cb72a1bc
Lukas Roehrich <lukasro@pik-potsdam.de>
2024-12-03 13:42:48 +0100

    reintroduce index page

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -47,6 +47,11 @@ html_sidebars = {
 }
 html_logo = 'resources/toad.png'
 html_permalinks_icon = "<span>¶</span>"                     # change the default anchor icon, paragraph mark only appears when hovering over the heading
+html_context = {
+    "url_mapping": {
+        "sidebar_main_nav_links.html'": "index.html",
+    }
+}
 
 # -- Function for sphinx extention linkcode -------------------------------
 

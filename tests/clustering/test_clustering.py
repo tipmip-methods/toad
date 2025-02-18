f4c5c54fa4ee823b4ca05691a94b7b9858cd6022
Jakob Harteg <github.0javt@aleeas.com>
2025-02-18 14:46:59 +0100

    Added Pytest for testing
    Wrote two basic tests to begin:
    - clustering (with healpix regridding)
    - shifts detection
    
    They can be executed in command line by
    $ cd toad
    $ pytest
    
    Note: Currently we get a warning regarding numpy which stems from the hdbscan import, see test_clustering.py for details.

---
+++ tests/clustering/test_clustering.py
@@ -0,0 +1,79 @@
+import pytest
+import numpy as np
+from toad import TOAD
+from sklearn.cluster import HDBSCAN # type: ignore
+from toad.regridding import HealPixRegridder
+
+
+@pytest.fixture
+def test_params():
+    """Fixture providing parameters for the clustering test.
+    Returns:
+        dict: A dictionary containing:
+            - lat (int): Latitude coarsening factor.
+            - lon (int): Longitude coarsening factor.
+            - min_cluster_size (int): Minimum size of clusters to be identified.
+            - shifts_threshold (float): Threshold for filtering shifts.
+            - expected_results (dict): Expected cluster counts for validation.
+            - expected_nside (int): Expected nside value for the regridder.
+    """
+    return {
+        "lat": 6,
+        "lon": 6,
+        "min_cluster_size": 25,
+        "shifts_threshold": 0.5,
+        "expected_results": {-1: 330245, 1: 2845, 0: 914, 2: 286, 3: 78},
+        "expected_nside": 16
+    }
+
+
+@pytest.fixture
+def toad_instance():
+    return TOAD("tutorials/test_data/global_mean_summer_tas.nc")
+
+
+def test_healpix_hdbscan(test_params, toad_instance):
+    """Test the HealPix HDBSCAN clustering method.
+
+    This test verifies the clustering of data using the HDBSCAN algorithm
+    after coarsening the data based on specified latitude and longitude
+    parameters. It also performs HealPix regridding to ensure the data
+    is appropriately structured for clustering. It checks that the actual
+    cluster counts and nside values match the expected results.
+
+    Args:
+        test_params (dict): Parameters for the test.
+        toad_instance (TOAD): Instance of TOAD containing the data.
+
+    Note: 
+        This warning caused by the HDBSCAN library may appear: "RuntimeWarning: numpy.ndarray size changed, may indicate binary incompatibility".
+        This is due to a known issue with the HDBSCAN library and numpy. https://github.com/scikit-learn-contrib/hdbscan/issues/457#issuecomment-1004296870
+    """
+
+    # Setup
+    td = toad_instance
+    td.data = td.data.coarsen(
+        lat=test_params["lat"], 
+        lon=test_params["lon"], 
+        boundary='trim'
+    ).mean()
+
+    # Test clustering
+    regridder = HealPixRegridder()
+    
+    td.compute_clusters(
+        "tas",
+        shifts_filter_func=lambda x: np.abs(x) > test_params["shifts_threshold"],
+        method=HDBSCAN(min_cluster_size=test_params["min_cluster_size"]),
+        overwrite=True,
+        regridder=regridder,
+    )
+
+    # Verify results
+    actual_counts = td.get_cluster_counts('tas')
+    
+    assert actual_counts == test_params["expected_results"], \
+        f"Expected {test_params['expected_results']}, got {actual_counts}"
+    
+    assert regridder.nside == test_params["expected_nside"], \
+        f"Expected nside {test_params['expected_nside']}, got {regridder.nside}"
---
+++ tests/shifts_detection/test_asdetect.py
@@ -0,0 +1,60 @@
+import pytest
+import numpy as np
+from toad import TOAD
+from toad.shifts_detection.methods import ASDETECT
+
+@pytest.fixture
+def test_params():
+    """Fixture providing parameters for the ASDETECT test.
+    
+    Returns:
+        dict: A dictionary containing:
+            - lat (int): Latitude coarsening factor.
+            - lon (int): Longitude coarsening factor.
+            - time (int): Time coarsening factor.
+            - expected_mean (float): Expected mean of the shifts.
+            - expected_std (float): Expected standard deviation of the shifts.
+    """
+    return {
+        "lat": 10,
+        "lon": 10,
+        "time": 3,
+        "expected_mean": 0.0038361712, 
+        "expected_std": 0.19410655,
+    }
+
+
+@pytest.fixture
+def toad_instance():
+    return TOAD("tutorials/test_data/global_mean_summer_tas.nc")
+
+
+def test_asdetect(test_params, toad_instance):
+    """Test the ASDETECT shift detection method.
+    
+    This test verifies the computation of shifts using the ASDETECT method
+    after coarsening the data based on specified latitude, longitude, and
+    time parameters. It checks that the computed mean and standard deviation
+    of the shifts match the expected results.
+
+    Args:
+        test_params (dict): Parameters for the test.
+        toad_instance (TOAD): Instance of TOAD containing the data.
+    """
+    # Setup
+    td = toad_instance
+    td.data = td.data.coarsen(
+        lat=test_params["lat"], 
+        lon=test_params["lon"], 
+        time=test_params["time"],
+        boundary='trim'
+    ).mean()
+
+    td.compute_shifts("tas", ASDETECT(), overwrite=True)
+    
+    shifts = td.get_shifts("tas")
+    mean = shifts.mean().values
+    std = shifts.std().values
+    
+    np.testing.assert_allclose(mean, test_params["expected_mean"], rtol=1e-5, atol=1e-8)
+    np.testing.assert_allclose(std, test_params["expected_std"], rtol=1e-5, atol=1e-8)

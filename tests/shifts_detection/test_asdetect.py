7e2b1747047748f964ebaff77e53553155b00202
RoehrichLukas <lukas-roehrich@web.de>
2025-05-06 11:55:16 +0200

    merging tests into one function

--- tests/shifts_detection/test_asdetect.py
+++ tests/shifts_detection/test_asdetect.py
@@ -25,42 +25,63 @@ def test_params():
         "expected_std": 0.19410655,
     }
 
-
 @pytest.fixture
 def toad_instance():
     return TOAD("tutorials/test_data/global_mean_summer_tas.nc")
 
-def test_asdetect_simple():
+
+
+def test_asdetect(test_params, toad_instance):
+    """Test the ASDETECT shift detection method.
+    
+    Two tests are performed:
+    1. A simple test with a known dataset to verify the correctness of the
+       shift detection method.
+    2. A test using a more realistic dataset to ensure the method works as expected
+       in practical scenarios.
+
+    Args:
+        test_params (dict): Parameters for the test.
+        toad_instance (TOAD): Instance of TOAD containing the data.
+
+    - Test 1 - additional notes:
+        Advantage of this test is that it can be easier to trace if
+        there is anything wrong with the mathematics of the implementation.
+        The given example can be computed by hand.
+
+        >> The exmaple here is:
+        data = [0, 0, 0, 1, 1, 1]       <- clear shift between index 2 and 3
+        l_min = 2                       <- minimum segmentation length
+        l_max = 3                       <- maximum segmentation length
+            -> this leads to two segmentation steps of the data:
+            i)  data_seg1 = [[0,0],[0,1],[1,1]]
+                -> gradients_seg1 = [0, 1 ,0]
+                -> median_seg1 = 0, MAD_seg1 = 0
+                -> detection_ts = [0, 0, 1, 1, 0, 0]
+            ii) data_seg2 = [[0,0,0],[1,1,1]]
+                -> gradients_seg2 = [0, 0]
+                -> median_seg2 = 0, MAD_seg2 = 0
+                -> detection_ts = [0, 0, 1, 1, 0, 0]    <- nothing changed
+            The shift values are divided by the number of segment lenghts:
+            -> shifts = [0, 0, 0.5, 0.5, 0, 0]
+
+        2025-04-30 NOTE: with the current implementation of ASDETECT.construct_detection_ts,
+                            the shifts values will actually be [0, 0, 0.5, 0, 0, 0]. This is
+                            due to a potential bug that has to be resolved.
+    
+    - Test 2 - additional notes:
+        This test verifies the computation of shifts using the ASDETECT method
+        after coarsening the data based on specified latitude, longitude, and
+        time parameters. It checks that the computed mean and standard deviation
+        of the shifts match the expected results.
     """
-    Test the ASDETECT shift detection method on a simple dataset
-    that can be calculated by hand.
 
-    Advantage of this test is that it can be easier to trace if
-    there is anything wrong with the mathematics of the implementation.
+    ##########################################
+    # test 1
 
-    The exmaple here is:
-    data = [0, 0, 0, 1, 1, 1]       <- clear shift between index 2 and 3
-    l_min = 2                       <- minimum segmentation length
-    l_max = 3                       <- maximum segmentation length
-        -> this leads to two segmentation steps of the data:
-        i)  data_seg1 = [[0,0],[0,1],[1,1]]
-            -> gradients_seg1 = [0, 1 ,0]
-            -> median_seg1 = 0, MAD_seg1 = 0
-            -> detection_ts = [0, 0, 1, 1, 0, 0]
-        ii) data_seg2 = [[0,0,0],[1,1,1]]
-            -> gradients_seg2 = [0, 0]
-            -> median_seg2 = 0, MAD_seg2 = 0
-            -> detection_ts = [0, 0, 1, 1, 0, 0]    <- nothing changed
-        The shift values are divided by the number of segment lenghts:
-        -> shifts = [0, 0, 0.5, 0.5, 0, 0]
-
-    2025-04-30 NOTE: with the current implementation of ASDETECT.construct_detection_ts,
-                        the shifts values will actuall be [0, 0, 0.5, 0, 0, 0]. This is
-                        due to a potential bug that still has to be resolved.
-    """
+    # - setup
     data_arr = np.array([0,0,0,1,1,1],dtype=float)
     time_arr = np.arange(len(data_arr))
-
     td = TOAD(
         xr.Dataset(
             {
@@ -74,24 +95,17 @@ def test_asdetect_simple():
         )
     )
 
+    # - call function
     td.compute_shifts("data", ASDETECT(lmin=2,lmax=3), overwrite=True)
     shifts = td.get_shifts("data").data[0,0]
     
+    # - compare results
     assert np.array_equal(shifts, np.array([0, 0, 0.5, 0, 0, 0], dtype=float))
 
-def test_asdetect(test_params, toad_instance):
-    """Test the ASDETECT shift detection method.
+    ##########################################
+    # test 2
 
-    This test verifies the computation of shifts using the ASDETECT method
-    after coarsening the data based on specified latitude, longitude, and
-    time parameters. It checks that the computed mean and standard deviation
-    of the shifts match the expected results.
-
-    Args:
-        test_params (dict): Parameters for the test.
-        toad_instance (TOAD): Instance of TOAD containing the data.
-    """
-    # Setup
+    # - setup
     td = toad_instance
     td.data = td.data.coarsen(
         lat=test_params["lat"],
@@ -100,11 +114,12 @@ def test_asdetect(test_params, toad_instance):
         boundary="trim",
     ).mean()
 
+    # - call function
     td.compute_shifts("tas", ASDETECT(), overwrite=True)
-
     shifts = td.get_shifts("tas")
     mean = shifts.mean().values
     std = shifts.std().values
 
+    # - compare results
     np.testing.assert_allclose(mean, test_params["expected_mean"], rtol=1e-5, atol=1e-8)
     np.testing.assert_allclose(std, test_params["expected_std"], rtol=1e-5, atol=1e-8)

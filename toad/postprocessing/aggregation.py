18246af92e5bb6d46ee87cd5c3df8376b7aed3d2
Jakob Harteg <github.0javt@aleeas.com>
2025-09-11 19:54:06 +0200

    Feat: cluster aggregation histogram

--- toad/postprocessing/aggregation.py
+++ toad/postprocessing/aggregation.py
@@ -1,21 +1,74 @@
-# TODO: write aggregation functions here: Combine clusters across different variables, datasets, etc
+import xarray as xr
+
+from toad.utils import get_unique_variable_name
 
 
 class Aggregation:
     """
     Aggregation methods for TOAD objects.
-
-    >> Note: TO BE IMPLEMENTED
     """
-
-    # TODO not sure how much of this should work through the toad class directly as we will want to combine many different datasets
 
     def __init__(self, toad):
         self.td = toad
 
-    def combine_clusters(self, var, cluster_ids, cluster_labels=None):
-        """
-        To be implemented: Combine clusters across different variables, datasets, etc.
-        """
+    def cluster_occurence_rate(
+        self,
+        cluster_vars: list[str] | None = None,
+    ) -> xr.DataArray:
+        """Calculate the normalized occurrence rate of points being part of any cluster.
 
-        raise NotImplementedError("Aggregation is not yet implemented.")
+        For each point in space, calculates how many times it is part of a cluster
+        (not noise) across different clustering variables, normalized by the total
+        number of clusterings. This is done by checking if each point was ever part
+        of a cluster (cluster label > -1) for each clustering variable, summing these
+        occurrences, and dividing by the total number of clustering variables.
+
+        Args:
+            cluster_vars: List of clustering variable names to consider. If None,
+                uses all clustering variables in the TOAD object. Each variable should
+                contain cluster labels where -1 indicates noise points and values >= 0
+                indicate cluster membership.
+
+        Returns:
+            DataArray containing the normalized cluster occurrence rate for each point.
+            Values range from 0 (never in a cluster) to 1 (always in a cluster).
+            The output variable name will be "cluster_occurence_rate" with a numeric
+            suffix if that name already exists in the dataset.
+
+        Example:
+            If a point is part of a cluster in 2 out of 3 clustering variables,
+            its occurrence rate would be 2/3 ≈ 0.67.
+        """
+        # Determine clustering variables
+        cluster_vars = cluster_vars if cluster_vars else self.td.cluster_vars
+
+        # Normalize by the total number of clusterings
+        num_clusterings = len(cluster_vars)
+        cluster_normalized = xr.where(
+            self.td.data[cluster_vars[0]].max(dim=self.td.time_dim) > -1,
+            1.0 / num_clusterings,
+            0,
+        )
+        # in-place summation to conserve memory
+        for cluster_var in cluster_vars[1:]:
+            cluster_normalized += xr.where(
+                self.td.data[cluster_var].max(dim=self.td.time_dim) > -1,
+                1.0 / num_clusterings,
+                0,
+            )
+
+        # Set name
+        output_label = get_unique_variable_name(
+            "cluster_occurence_rate", self.td.data, self.td.logger
+        )
+        cluster_normalized = cluster_normalized.rename(output_label)
+
+        # Add attributes
+        cluster_normalized.attrs.update(
+            {
+                "cluster_vars": cluster_vars,
+                "description": "Normalized occurrence rate of points being part of any cluster",
+            }
+        )
+
+        return cluster_normalized

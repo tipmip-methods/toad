f171659b0a2fb118086d04b6fcfc070bafa6b189
Jakob Harteg <github.0javt@aleeas.com>
2026-01-20 10:27:41 +0100

    Expose toad version

--- toad/__init__.py
+++ toad/__init__.py
@@ -4,11 +4,19 @@
 
 # Import warnings configuration first (must be before cartopy import)
 import toad._warnings  # noqa: F401
+from toad._version import __version__
 from toad.clustering import compute_clusters
 from toad.core import TOAD
 from toad.plotting import MapStyle, Plotter
 from toad.shifts import compute_shifts
 
-__all__ = ["TOAD", "compute_shifts", "compute_clusters", "Plotter", "MapStyle"]
+__all__ = [
+    "TOAD",
+    "compute_shifts",
+    "compute_clusters",
+    "Plotter",
+    "MapStyle",
+    "__version__",
+]
 
 # ================================================
